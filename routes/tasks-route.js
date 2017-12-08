0c30c8e04231424c15f1e9cf6405a700a2b2ed6c
Nayema Nasir <nayema.nasir89@gmail.com>
2017-12-07 20:48:00 -0500

    Extracts a controller class from the tasks route

--- routes/tasks-route.js
+++ routes/tasks-route.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET task list */
-router.get('/', function (req, res, next) {
-  res.render('tasks/index')
-})
+class TasksController {
+  static getTaskList (req, res) {
+    res.render('tasks/index')
+  }
 
-/* POST new task */
-router.post('/', function (req, res, next) {
-  let newTaskName = req.body['new-task-name']
-  res.render('tasks/index', { tasks: [newTaskName] })
-})
+  static createTask (req, res) {
+    let newTaskName = req.body['new-task-name']
+    res.render('tasks/index', { tasks: [newTaskName] })
+  }
+}
+
+router.get('/', TasksController.getTaskList)
+router.post('/', TasksController.createTask)
 
 module.exports = router
