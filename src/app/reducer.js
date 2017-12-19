bcc84b2b9edc3334736b594028f55144ab08fe52
Nayema Nasir <nayema.nasir89@gmail.com>
2017-12-19 15:00:41 -0500

    Revert back to 5bae5ee

--- src/app/reducer.js
+++ src/app/reducer.js
@@ -12,9 +12,7 @@ function reducer (state = BLANK_STATE, action) {
     }
     case ActionTypes.DELETE_TASK: {
       const oldTasks = state.tasks
-      const newTasks = oldTasks.slice()
-      const index = oldTasks.indexOf(action.payload.taskName)
-      newTasks.splice(index, 1)
+      const newTasks = oldTasks.filter((task) => task !== action.payload.taskName)
       return {
         tasks: newTasks
       }
