b35a4aa0b46617b64484f306acafacc12620a8d9
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-14 22:45:35 -0500

    Add the ability to filter tasks by authenticated user

--- modules/tasks/TasksController.js
+++ modules/tasks/TasksController.js
@@ -2,14 +2,17 @@ const Task = require('./Task')
 
 class TasksController {
   static async all (req, res) {
-    const tasks = await Task.query()
+    const tasks = await Task
+      .query()
+      .where('user_id', '=', req.user.sub)
     return res.send(tasks)
   }
 
   static async add (req, res) {
     const task = await Task.query().insert({
       'name': req.body['name'],
-      'category': req.body['category']
+      'category': req.body['category'],
+      'user_id': req.user.sub
     })
     return res.send(task)
   }
@@ -19,6 +22,7 @@ class TasksController {
       .query()
       .delete()
       .where('id', '=', req.body['id'])
+      .andWhere('user_id', '=', req.user.sub)
     return res.end()
   }
 }
--- modules/tasks/tasks.spec.js
+++ modules/tasks/tasks.spec.js
@@ -9,49 +9,87 @@ describe('tasks', () => {
     await Task.query().delete()
   })
 
-  it('gets all the tasks', async () => {
-    await Task.query().insert({ 'name': 'Some Task', 'category': 'Some Category' })
+  describe('when getting all tasks', () => {
+    it('gets all', async () => {
+      await Task.query().insert({ 'name': 'Some Task', 'category': 'Some Category', 'user_id': 'test-user' })
 
-    const response = await request(app)
-      .get('/tasks/')
-      .set('Authorization', 'Bearer ' + testJwt)
+      const response = await request(app)
+        .get('/tasks/')
+        .set('Authorization', 'Bearer ' + testJwt)
 
-    expect(response.statusCode).toBe(200)
-    const tasks = response.body
-    expect(tasks[0]).toHaveProperty('id')
-    expect(tasks[0]).toEqual(expect.objectContaining({ 'name': 'Some Task', 'category': 'Some Category' }))
+      expect(response.statusCode).toBe(200)
+      const tasks = response.body
+      expect(tasks[0]).toHaveProperty('id')
+      expect(tasks[0]).toEqual(expect.objectContaining({
+        'name': 'Some Task',
+        'category': 'Some Category',
+        'user_id': 'test-user'
+      }))
+    })
+
+    it('does not get another user task', async () => {
+      await Task.query().insert({ 'name': 'Some Task', 'category': 'Some Category', 'user_id': 'another-user' })
+
+      const response = await request(app)
+        .get('/tasks/')
+        .set('Authorization', 'Bearer ' + testJwt)
+
+      expect(response.statusCode).toBe(200)
+      const tasks = response.body
+      expect(tasks.length).toEqual(0)
+    })
   })
 
-  it('adds a new task', async () => {
-    const task = { 'name': 'Some Task', 'category': 'Some Category' }
+  describe('when adding a new task', () => {
+    it('adds', async () => {
+      const task = { 'name': 'Some Task', 'category': 'Some Category' }
 
-    const response = await request(app)
-      .post('/tasks/')
-      .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer ' + testJwt)
-      .send(task)
+      const response = await request(app)
+        .post('/tasks/')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ' + testJwt)
+        .send(task)
 
-    expect(response.statusCode).toBe(200)
-    const taskResponse = response.body
-    expect(taskResponse).toHaveProperty('id')
-    expect(taskResponse).toEqual(expect.objectContaining({
-      'name': 'Some Task',
-      'category': 'Some Category'
-    }))
+      expect(response.statusCode).toBe(200)
+      const taskResponse = response.body
+      expect(taskResponse).toHaveProperty('id')
+      expect(taskResponse).toEqual(expect.objectContaining({
+        'name': 'Some Task',
+        'category': 'Some Category',
+        'user_id': 'test-user'
+      }))
+    })
   })
 
-  it('removes an existing task', async () => {
-    await Task.query().insert({ 'id': 999, 'name': 'XXXXX', 'category': 'XXXXX' })
-    const task = { 'id': 999 }
+  describe('when removing an existing task', () => {
+    it('removes', async () => {
+      await Task.query().insert({ 'id': 999, 'name': 'XXXXX', 'category': 'XXXXX', 'user_id': 'test-user' })
+      const task = { 'id': 999 }
 
-    const response = await request(app)
-      .delete('/tasks/')
-      .set('Content-Type', 'application/json')
-      .set('Authorization', 'Bearer ' + testJwt)
-      .send(task)
+      const response = await request(app)
+        .delete('/tasks/')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ' + testJwt)
+        .send(task)
 
-    expect(response.statusCode).toBe(200)
-    const results = await Task.query().count()
-    expect(results[0]['count']).toEqual('0')
+      expect(response.statusCode).toBe(200)
+      const results = await Task.query().count()
+      expect(results[0]['count']).toEqual('0')
+    })
+
+    it('does not remove another user task', async () => {
+      await Task.query().insert({ 'id': 999, 'name': 'XXXXX', 'category': 'XXXXX', 'user_id': 'another-user' })
+      const task = { 'id': 999 }
+
+      const response = await request(app)
+        .delete('/tasks/')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', 'Bearer ' + testJwt)
+        .send(task)
+
+      expect(response.statusCode).toBe(200)
+      const results = await Task.query().count()
+      expect(results[0]['count']).toEqual('1')
+    })
   })
 })
