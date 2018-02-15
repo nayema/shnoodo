bf9f44ef20e1f3ca57112d5d12443cf9213ca14f
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-14 21:43:53 -0500

    Extract test jwt to separate file

--- modules/tasks/tasks.spec.js
+++ modules/tasks/tasks.spec.js
@@ -1,31 +1,8 @@
 import request from 'supertest'
 
-const jwt = require('jsonwebtoken')
-const pem2jwk = require('pem-jwk').pem2jwk
-const keypair = require('keypair')
-const nock = require('nock')
-
 import app from '../../app'
 import Task from './Task'
-
-const pair = keypair()
-
-const publicJWK = pem2jwk(pair.public)
-publicJWK.use = 'sig'
-publicJWK.kid = 'kid'
-
-nock(process.env.JWKS_URI)
-  .get('')
-  .reply(200, {
-    keys: [publicJWK]
-  })
-
-const token = jwt.sign({ sub: 'test-user' }, pair.private, {
-  algorithm: 'RS256',
-  header: { kid: 'kid' },
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_ISSUER
-})
+import testJwt from './test-jwt'
 
 describe('tasks', () => {
   beforeEach(async () => {
@@ -37,7 +14,7 @@ describe('tasks', () => {
 
     const response = await request(app)
       .get('/tasks/')
-      .set('Authorization', 'Bearer ' + token)
+      .set('Authorization', 'Bearer ' + testJwt)
 
     expect(response.statusCode).toBe(200)
     const tasks = response.body
