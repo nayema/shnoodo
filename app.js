ede0d2a85edf9cc020d652240db41a29a6461775
Nayema Nasir <nayema.nasir89@gmail.com>
2018-02-17 21:46:14 -0500

    Setup heroku deployment

--- app.js
+++ app.js
@@ -29,30 +29,26 @@ const jwtCheck = jwt({
 
 app.use('/tasks', jwtCheck, tasksRouter)
 
-// const router = express.Router()
+const router = express.Router()
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
-// router.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-// })
-// app.use('/', router)
+app.use(express.static(path.join(__dirname, 'client/build')))
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+app.use('/', router)
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
-// error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
-  res.render('error')
+  res.send(err)
 })
 
 module.exports = app
